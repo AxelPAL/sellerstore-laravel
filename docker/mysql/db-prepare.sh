d5d7f1c693b3068033f95e1f72fc1b5f3d8116cd
AxelPAL <axelpal@gmail.com>
2021-07-31 23:51:25 +0500

    CI/CD WIP

--- docker/mysql/db-prepare.sh
+++ docker/mysql/db-prepare.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker-compose -f docker-compose.ci.yml exec mysql mysql -e 'create database if not exists sellerstore;'
-docker-compose -f docker-compose.ci.yml run --entrypoint="" app ./artisan migrate
+docker-compose -f docker-compose.ci.yml run --entrypoint="" app ./artisan migrate --force
