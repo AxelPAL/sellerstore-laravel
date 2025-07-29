6de6a5f0139a783792bb96ea28ebd8936b170b5e
AxelPAL <axelpal@gmail.com>
2025-07-29 16:27:35 +0500

    ci fix

--- docker/mysql/db-prepare.sh
+++ docker/mysql/db-prepare.sh
@@ -2,4 +2,4 @@
 
 sleep 10
 docker compose -f docker-compose.ci.yml exec mysql mysql -e 'create database if not exists sellerstore;'
-docker compose -f docker-compose.ci.yml exec --entrypoint="" app ./artisan migrate --force
+docker compose -f docker-compose.ci.yml run --entrypoint="" app ./artisan migrate --force
